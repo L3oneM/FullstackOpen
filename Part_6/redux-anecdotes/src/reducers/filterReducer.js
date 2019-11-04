f82aec6f43003ac8fc5a0575bf1e52444719dd53
L3oneMclear <apoWebDevAcc@gmail.com>
2019-11-05 00:17:45 +0200

    check

--- Part_6/redux-anecdotes/src/reducers/filterReducer.js
+++ Part_6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,7 +1,6 @@
 const filterReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_FILTER':
-      console.log(23, action.filter)
       return action.filter
     default:
       return state
